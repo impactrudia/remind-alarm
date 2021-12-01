3ec2afc80bea293446e93d1b671323d2f8b282cd
impactrudia <impactyoung@gmail.com>
2021-12-01 23:14:53 +0900

    initial project

---
+++ RemindAlarm/app/src/main/java/com/impactrudia/remindalarm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.impactrudia.remindalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
