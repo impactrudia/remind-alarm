025f3cfda7e8f75e861f5b52f5e57d82823815ba
impactrudia <impactyoung@gmail.com>
2021-12-02 18:30:25 +0900

    3_리마인더 알림 화면 (#7)
    * feat: 리마인드 알림 UI
    
    * feat: 전체화면

---
+++ RemindAlarm/app/src/main/java/com/impactrudia/remindalarm/presentation/ui/notice/ReminderNoticeFragment.kt
@@ -0,0 +1,24 @@
+package com.impactrudia.remindalarm.presentation.ui.notice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.impactrudia.remindalarm.databinding.FragmentReminderNoticeBinding
+
+class ReminderNoticeFragment : Fragment() {
+
+    private lateinit var binding: FragmentReminderNoticeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentReminderNoticeBinding.inflate(inflater, container, false)
+
+        binding.apply {
+        }
+        return binding.root
+    }
+}
